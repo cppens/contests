ad9390f898926c77fedf9e54d586ef6bc28432e7
Aldo Faiz Winarno <71934691+aldofaiz@users.noreply.github.com>
2020-10-07 06:30:34 +0700

    Update The-Atomic-Swordsman.cpp

--- Techphoria 2020/Qualification/Solutions/The-Atomic-Swordsman.cpp
+++ Techphoria 2020/Qualification/Solutions/The-Atomic-Swordsman.cpp
@@ -1,7 +1,21 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+// Test Case :
+/*
+    1 1     menang          2 1     menang          3 1     menang          4 1     menang
+    1 2     kalah           2 2     menang          3 2     menang          4 2     menang
+    1 3     kalah           2 3     kalah           3 3     menang          4 3     menang
+                            2 4     kalah           3 4     menang          4 4     menang
+                                                    3 5     kalah           4 5     menang
+                                                    3 6     kalah           4 6     menang
+                                                                            4 7     menang
+                                                                            4 8     menang
+                                                                            4 9     kalah
+                                                                            4 10    kalah
+                                                                            
+   Batas menang sesuai tebasan : 1 , 2 , 4 , 8 , ...
+*/
 int main()
 {
     double N,M;
